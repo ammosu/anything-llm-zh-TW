15b78335bbea89380138db888d9809b1654cb035
ammosu <pang2258@gmail.com>
2024-12-04 16:33:47 +0800

    language zh

--- frontend/src/hooks/useLanguageOptions.js
+++ frontend/src/hooks/useLanguageOptions.js
@@ -1,11 +1,11 @@
 import i18n from "@/i18n";
 import { resources as languages } from "@/locales/resources";
+import { useTranslation } from "react-i18next";
 
 export function useLanguageOptions() {
   const supportedLanguages = Object.keys(languages);
-  const languageNames = new Intl.DisplayNames(supportedLanguages, {
-    type: "language",
-  });
+  const { t } = useTranslation();
+
   const changeLanguage = (newLang = "en") => {
     if (!Object.keys(languages).includes(newLang)) return false;
     i18n.changeLanguage(newLang);
@@ -14,7 +14,13 @@ export function useLanguageOptions() {
   return {
     currentLanguage: i18n.language || "en",
     supportedLanguages,
-    getLanguageName: (lang = "en") => languageNames.of(lang),
+    getLanguageName: (lang = "en") => {
+      // Fallback to default Intl.DisplayNames if no translation is found
+      const languageNames = new Intl.DisplayNames([i18n.language || "en"], {
+        type: "language",
+      });
+      return t(`languages.${lang}`, { defaultValue: languageNames.of(lang) });
+    },
     changeLanguage,
   };
 }
